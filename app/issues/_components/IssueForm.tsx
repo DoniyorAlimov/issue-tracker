609608c86ada5f62ee4d8a8b2f2d864785cc1c66
Doniyor Alimov <alimov.doniyor28@gmail.com>
2023-10-11 20:40:30 +0500

    Update Issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -38,8 +38,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
-      router.push("/issues/list");
-      router.refresh();
+      router.push("/issues");
     } catch (error) {
       setSubmitting(false);
       setError("An unexpected error occurred.");
